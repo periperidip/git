339ba75d4a27add0d5cf60a65a8c44f4b4e8d7d0
Shourya Shukla <shouryashukla.oo@gmail.com>
2020-06-30 18:04:36 +0530

    submodule: apply Christian's suggestions
    Apply Christian's suggestions to make the patch even better. The
    suggestions were:
        1. remove the 'static' from the enum.
        2. pass diff_cmd as an argument to 'compute_..()' since it is only
        used in this function and module_summary().
        3. change  diff_cmd++ to diff_cmd = DIFF_FILES
    
    The make is successful and tests t7401 t7418 pass.

--- builtin/submodule--helper.c
+++ builtin/submodule--helper.c
@@ -955,10 +955,10 @@ struct summary_cb {
 };
 #define SUMMARY_CB_INIT { 0, NULL, NULL, 0, 0, 0, 0, 0 }
 
-static enum {
+enum diff_cmd_info {
 	DIFF_INDEX,
 	DIFF_FILES
-} diff_cmd = DIFF_INDEX;
+};
 
 static int verify_submodule_object_name(const char *sm_path, const char *sha1)
 {
@@ -1226,7 +1226,7 @@ static void submodule_summary_callback(struct diff_queue_struct *q,
 	}
 }
 
-static int compute_summary_module_list(char *head, struct summary_cb *info)
+static int compute_summary_module_list(char *head, struct summary_cb *info, enum diff_cmd_info diff_cmd)
 {
 	struct argv_array diff_args = ARGV_ARRAY_INIT;
 	struct rev_info rev;
@@ -1284,6 +1284,7 @@ static int module_summary(int argc, const char **argv, const char *prefix)
 	int summary_limit = -1;
 	struct child_process cp_rev = CHILD_PROCESS_INIT;
 	struct strbuf sb = STRBUF_INIT;
+	enum diff_cmd_info diff_cmd = DIFF_INDEX;
 	int ret;
 
 	struct option module_summary_options[] = {
@@ -1334,7 +1335,7 @@ static int module_summary(int argc, const char **argv, const char *prefix)
 	if (files) {
 		if (cached)
 			die(_("The --cached option cannot be used with the --files option"));
-		diff_cmd++;
+		diff_cmd = DIFF_FILES;
 	}
 
 	info.argc = argc;
@@ -1346,7 +1347,7 @@ static int module_summary(int argc, const char **argv, const char *prefix)
 	info.files = files;
 	info.summary_limit = summary_limit;
 
-	ret = compute_summary_module_list(diff_cmd ? NULL : sb.buf, &info);
+	ret = compute_summary_module_list(diff_cmd ? NULL : sb.buf, &info, diff_cmd);
 	strbuf_release(&sb);
 	return ret;
 }
