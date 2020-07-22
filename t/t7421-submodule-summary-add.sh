e08e5f1bfa0999f3ffa3403d81b3cbb8c6e23bcc
Shourya Shukla <shouryashukla.oo@gmail.com>
2020-07-23 01:04:08 +0530

    t7421: summary test environment setup
    Setup the minimal environment for testing 'git submodule summary',
    consisting of a submodule and a superproject housing the submodule.
    
    Mentored-by: Christian Couder <chriscool@tuxfamily.org>
    Mentored-by: Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
    Signed-off-by: Shourya Shukla <shouryashukla.oo@gmail.com>

--- t/t7421-submodule-summary-add.sh
+++ t/t7421-submodule-summary-add.sh
@@ -11,3 +11,14 @@ while making sure to add submodules using `git submodule add` instead of
 '
 
 . ./test-lib.sh
+
+test_expect_success 'summary test environment setup' '
+	git init sm &&
+	test_commit -C sm "add file" file file-content file-tag &&
+
+	git submodule add ./sm submodule &&
+	test_tick &&
+	git commit -m "add submodule"
+'
+
+test_done
