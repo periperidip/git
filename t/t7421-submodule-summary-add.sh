563f9eb3071504d5fa0fbe466ba07c12d06faf7b
Shourya Shukla <shouryashukla.oo@gmail.com>
2020-07-23 00:59:35 +0530

    t7421: Initialise test for verifying summary support for submodules
    Initialise test 't7421-submodule-summary-add.sh' for verifying summary
    support for submodules by adding them using 'git submodule add' instead
    of mimicing t7401 by using 'git add'.
    
    Mentored-by: Christian Couder <chriscool@tuxfamily.org>
    Mentored-by: Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
    Signed-off-by: Shourya Shukla <shouryashukla.oo@gmail.com>

---
+++ t/t7421-submodule-summary-add.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# Copyright (C) 2020  Shourya Shukla
+#
+
+test_description='Summary support for submodules, adding them using git submodule add
+
+This test tries to verify the sanity of summary subcommand of git submodule
+while making sure to add them using git submodule add instead of the way it
+is done using git add in t7401 by Ping Yin.
+'
+
+TEST_NO_CREATE_REPO=1
+. ./test-lib.sh
